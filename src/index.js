054073a94382724467018a250e1a295984e9a943
Ryan <jragard@gmail.com>
2018-06-19 11:17:38 -0400

    redux setup done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './reducer.js'
+import App from './App.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+let store = createStore(reducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
+
+// class App extends Component {
+//     render() {
+//       return (
+//         <div className="App">
+//           <header className="App-header">
+            
+//             <h1 className="App-title">Welcome to React</h1>
+//           </header>
+//           <p className="App-intro">
+//             To get started, edit <code>src/App.js</code> and save to reload.
+//           </p>
+//         </div>
+//       );
+//     }
+//   }
+
+export default store;
